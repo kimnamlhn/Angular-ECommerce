789ebf1b9bfd925fd2d6651c8140c54a9e189bc0
kimnamlhn <kimnam.cpc@gmail.com>
2021-08-13 13:16:10 +0700

    components structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './components/header/header.component';
+import { SlidebarComponent } from './components/slidebar/slidebar.component';
+import { ProductComponent } from './components/product/product.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SlidebarComponent,
+    ProductComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NoopAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
